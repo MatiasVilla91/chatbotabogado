176352371bdcde0552fa2e6bb31f8f78a1fa49f9
Mati <180681mvs@gmail.com>
2025-03-10 04:17:09 -0300

    usuarios.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
-}, { timestamps: true });
+}, { timestamps: true,
+    collection: 'usuarios' // 👈 Forzar el nombre de la colección
+ });
 
 // Especificamos explícitamente la colección 'usuarios'
 module.exports = mongoose.model('User', UserSchema, 'usuarios');
